5e504fa2e920f46d999afb3fa09324e2c6686798
natashalballard <nbitcareer@gmail.com>
2018-01-31 09:45:38 -0500

    Initial commit

---
+++ src/main/java/com/springboot_10/demo/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.springboot_10.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+}
---
+++ src/main/java/com/springboot_10/demo/SpringBoot10Application.java
@@ -0,0 +1,12 @@
+package com.springboot_10.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot10Application.class, args);
+	}
+}
